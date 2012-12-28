960f573e943b2d433ea841e04852e0abd9ccbb23
Elmer Bulthuis <elmerbulthuis@gmail.com>
2012-12-28 20:49:43 +0100

    cleanup tests

--- test/void.js
+++
@@ -1,9 +0,0 @@
-var Tokenizer = require('../lib/3kenizer').Tokenizer;
-
-module.exports['test'] = function(beforeExit, assert){
-	assert.equal(1, 1);
-
-	beforeExit(function(){
-		assert.equal(1, 1);
-	});
-};
