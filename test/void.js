31046ad0ed63997341238b89454c93799f2e1820
Elmer Bulthuis <elmerbulthuis@gmail.com>
2012-12-28 20:34:20 +0100

    initial class

--- test/void.js
+++ test/void.js
@@ -1,4 +1,8 @@
+var Tokenizer = require('../lib/3kenizer').Tokenizer;
+
 module.exports['test'] = function(beforeExit, assert){
+	assert.equal(1, 1);
+
 	beforeExit(function(){
 		assert.equal(1, 1);
 	});
