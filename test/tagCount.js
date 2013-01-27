6aacc9f5671205b838dafda23152b4d8c9af40b2
Elmer Bulthuis <elmerbulthuis@gmail.com>
2013-01-27 13:58:06 +0100

    xml tests

--- test/tagCount.js
+++ test/tagCount.js
@@ -2,7 +2,32 @@ var fs = require('fs');
 var path = require('path');
 var Tokenizer = require('../lib/3kenizer').Tokenizer;
 
-module.exports['html'] = test('html.html', null);
+module.exports['lorem'] = test('lorem.html', {
+	html: 1
+	, head: 1
+	, title: 1
+	, body: 1
+	, br: 4
+});
+module.exports['r_and_j'] = test('r_and_j.xml', {
+	PLAY: 1
+	, TITLE: 33
+	, FM: 1
+	, P: 4
+	, PERSONAE: 1
+	, PERSONA: 25
+	, PGROUP: 3
+	, GRPDESCR: 3
+	, SCNDESCR: 1
+	, PLAYSUBT: 1
+	, ACT: 5
+	, PROLOGUE: 2
+	, SPEECH: 841
+	, SPEAKER: 841
+	, LINE: 3093
+	, SCENE: 24
+	, STAGEDIR: 202
+});
 
 function test(file, expect){
 	return function(beforeExit, assert){
@@ -12,7 +37,9 @@ function test(file, expect){
 		var tokenizer = new Tokenizer();
 		tokenizer.expressions = {
 			'whitespace': /\s+/g
-			, 'openTag':	/<\s*([A-Za-z0-9\:]+)\s*>/g
+			, 'tagBegin':	/<\s*([A-Za-z0-9\:]+)\s*/g
+			, 'openTagEnd': /\s*>/
+			, 'atomicTagEnd': /\s*\/>/
 			, 'closeTag':	/<\s*\/([A-Za-z0-9\:]+)\s*>/g			
 			, 'other': Tokenizer.specialExpressions.other
 			, 'end': Tokenizer.specialExpressions.end
@@ -23,68 +50,96 @@ function test(file, expect){
 		readStream.on('close', readStream_close);
 
 		beforeExit(function(){
-			console.log(tokens);
-			console.log(tags);
+			//console.log(tokens);
+			//console.log(tags);
+			assert.deepEqual(tags, expect);
 			assert.equal(tokens.end, 1);
 		});
 
 	
-		parseDocument();
+		parseNext();
 
 		readStream.pipe(tokenizer);
 
 
-		function parseDocument(){
-			var tags = [];
 
-			parseNext();
+		function parseNext(){
+			tokenizer.addHandler(
+				['whitespace', 'tagBegin', 'closeTag', 'end', 'other']
+				, handleNext
+			);
+		}//parseNext
+		
+		function handleNext(match, categories){
+			tokenizer.addToken(match.category);
 
-			function parseNext(){
-				if(tags.length === 0){
-					tokenizer.addHandler(
-						['whitespace', 'openTag', 'end', 'other']
-						, handler
-					);
-				}
-				else{
-					tokenizer.addHandler(
-						['whitespace', 'openTag', 'closeTag', 'other']
-						, handler
-					);
-				}
-			}//parseNext
+			switch(match.category){
+				case 'end':
+				tokenizer.addHandler([], null);
+				break;
 
-			function handler(match, categories){
-				tokenizer.addToken(match.category);
+				case 'whitespace':
+				case 'other':
+				parseNext();
+				break;
 
-				switch(match.category){
-					case 'end':
-					tokenizer.addHandler([], null);
-					break;
+				case 'tagBegin':
+				tokenizer.addToken('tag', match[1]);
 
-					case 'whitespace':
-					case 'other':
-					parseNext();
-					break;
+				parseOpenTag();
+				parseAtomicTag();
+				break;
 
-					case 'openTag':
-					tokenizer.addToken('tag', match[1]);
-
-					tags.push(match[1]);
-
-					parseNext();
-					break;
-
-					case 'closeTag':
-					if(tags.pop() !== match[1]) throw "tag '" + match[1] + "' not closed";
-
-					parseNext();
-					break;
-				}
-			}//handler
+				case 'closeTag':
+				parseNext();
+				break;
+			}
+		}//handleNext
 
 
-		}//parseDocument
+		function parseOpenTag(){
+			tokenizer.addHandler(
+				['whitespace', 'openTagEnd']
+				, handleOpenTag
+			);
+		}//parseOpenTag
+
+		function handleOpenTag(match, categories){
+			tokenizer.addToken(match.category);
+
+			switch(match.category){
+				case 'whitespace':
+				parseOpenTag();
+				break;
+
+				case 'openTagEnd':
+				parseNext();
+				break;
+			}
+		}//handleOpenTag
+
+
+		function parseAtomicTag(){
+			tokenizer.addHandler(
+				['whitespace', 'atomicTagEnd']
+				, handleAtomicTag
+			);
+		}//parseAtomicTag
+
+		function handleAtomicTag(match, categories){
+			tokenizer.addToken(match.category);
+
+			switch(match.category){
+				case 'whitespace':
+				parseAtomicTag();
+				break;
+
+				case 'atomicTagEnd':
+				parseNext();
+				break;
+			}
+		}//handleAtomicTag
+
 
 
 
