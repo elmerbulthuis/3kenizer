9d8b5474169f771573574ddda0684566d2380297
Elmer Bulthuis <elmerbulthuis@gmail.com>
2013-01-27 12:29:27 +0100

    introducing 'other' special expression

--- test/wordCount.js
+++ test/wordCount.js
@@ -5,6 +5,7 @@ var Tokenizer = require('../lib/3kenizer').Tokenizer;
 module.exports['countOneTwoThree'] = test('oneTwoThree.txt', 3);
 module.exports['countLorem'] = test('lorem.txt', 69);
 module.exports['pg135'] = test('pg135.txt', 577599);
+module.exports['html'] = test('html.html', 9);
 
 function test(file, expect){
 	return function(beforeExit, assert){
@@ -14,12 +15,12 @@ function test(file, expect){
 		tokenizer.expressions = {
 			'word': /\w+/g
 			, 'whitespace': /\s+/g
-			, 'other': /[^\s\w]+/g
 			, 'exclamation': '!'
 			, 'question': '?'
-			, 'eof': Tokenizer.EOF
+			, 'other': Tokenizer.specialExpressions.other
+			, 'end': Tokenizer.specialExpressions.end
 		}
-		var categories = ['question', 'exclamation', 'whitespace', 'word', 'other', 'eof'];
+		var categories = ['question', 'exclamation', 'whitespace', 'word', 'end', 'other'];
 		tokenizer.addHandler(categories, tokenizer_handler);
 		tokenizer.on('token', tokenizer_token);
 
@@ -30,7 +31,7 @@ function test(file, expect){
 		beforeExit(function(){
 			//console.log(counters);
 			assert.equal(counters.word, expect);
-			assert.equal(counters.eof, 1);
+			assert.equal(counters.end, 1);
 		});
 
 		readStream.pipe(tokenizer);
@@ -48,7 +49,7 @@ function test(file, expect){
 		function tokenizer_handler(match){
 			tokenizer.addToken(match.category);
 			switch(match.category){
-				case 'eof':
+				case 'end':
 				tokenizer.addHandler([], null);
 				break;
 
